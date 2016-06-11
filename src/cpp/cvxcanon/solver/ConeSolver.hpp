d7a5dde1446b93c9b3324f5dc4669512aa2f829a
fabioftv <fabioftv@k-state.edu>
2016-06-11 09:34:50 -0500

    Updated ConeConstraint and Parameters for ECOS

--- src/cpp/cvxcanon/solver/ConeSolver.hpp
+++ src/cpp/cvxcanon/solver/ConeSolver.hpp
@@ -1,50 +1,68 @@
-// Interface for cone solvers
+// Interface for Cone Solvers
 //
 // TODO(mwytock): This should likely be a pure C interface to ease
 // implementation for existing cone solvers (ECOS, SCS)
 
+// See: http://mathprogbasejl.readthedocs.io/en/latest/conic.html
+
 #ifndef CONE_SOLVER_H
 #define CONE_SOLVER_H
 
-#include <Eigen/Core>
 #include <Eigen/Sparse>
 
 #include "cvxcanon/solver/SolverStatus.hpp"
 #include "cvxcanon/util/Utils.hpp"
 
+// TODO(fabioftv): Need another version of SymbolicConeSolver to account for other Cones
 class ConeConstraint {
- public:
-  enum Cone {
-    ZERO,
-    NON_NEGATIVE,
-    SECOND_ORDER,
-  };
+public:
+	enum Cone {
+		FREE,						// No Restrictions
+		ZERO,						// All Components = 0
+		NON_NEGATIVE,				// Nonnegative Orthant
+		NON_POSITIVE,				// Nonpositive Orthant
+		SECOND_ORDER,				// Second Order Cone
+		ROTATED_SECOND_ORDER,		// Rotated Second Order Cone
+		SYM_POS_SEMI,				// Symmetric Positive Semidefinite Matrices
+		PRIMAL_EXPO,				// Primal Exponential Cone
+		DUAL_EXPO					// Dual Exponential Cone
+	};
 
-  Cone cone;
-  int offset, size;
+	Cone cone;
+	int offset_eq, offset_leq, size_eq, size_leq;
 };
 
-// A cone problem of the form
-// minimize    c'x
-// subject to  b - Ax in K
 class ConeProblem {
- public:
-  SparseMatrix A;
-  DenseVector b, c;
-  std::vector<ConeConstraint> constraints;
+
+// SCS
+// Minimize		c'x
+// Subject to:	Ax + s = b
+//				s in K
+
+// ECOS
+// Minimize		c'x
+// Subject to:	Ax = b
+//				h - Gx in K
+
+public:
+	SparseMatrix A;
+	SparseMatrix G;
+	DenseVector b, h, c;
+	std::vector<ConeConstraint> constraints_eq;
+	std::vector<ConeConstraint> constraints_leq;
 };
 
 class ConeSolution {
- public:
-  SolverStatus status;
-  DenseVector x, y;
-  double objective_value;
+public:
+	SolverStatus status;
+	DenseVector x, y;
+	double objective_value;
 };
 
 class ConeSolver {
- public:
-  virtual ~ConeSolver() {}
-  virtual ConeSolution solve(const ConeProblem& problem) = 0;
+public:
+	virtual ConeSolution solve(const ConeProblem& problem) = 0;
+	virtual ~ConeSolver() {}
 };
 
 #endif  // CONE_SOLVER_H
