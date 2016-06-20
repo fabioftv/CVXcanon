b935030710e8bc0a255a3029ecc66a5197fb436b
fabioftv <fabioftv@k-state.edu>
2016-06-20 17:24:37 -0500

    Updated Test Files

--- src/cpp/cvxcanon/expression/TextFormatTest.cpp
+++ src/cpp/cvxcanon/expression/TextFormatTest.cpp
@@ -1,8 +1,8 @@
 
-/*
+
 #include "gtest/gtest.h"
 
 TEST(TextFormatTest, Names) {
 
 }
-*/
+
--- src/cpp/cvxcanon/util/TestMain.cpp
+++ src/cpp/cvxcanon/util/TestMain.cpp
@@ -1,7 +1,7 @@
 
-//#include "glog/logging.h"
-//#include "gtest/gtest.h"
-/*
+#include "glog/logging.h"
+#include "gtest/gtest.h"
+
 GTEST_API_ int main(int argc, char **argv) {
   testing::InitGoogleTest(&argc, argv);
   google::InitGoogleLogging(argv[0]);
@@ -9,4 +9,3 @@ GTEST_API_ int main(int argc, char **argv) {
   int retval = RUN_ALL_TESTS();
   return retval;
 }
-*/
