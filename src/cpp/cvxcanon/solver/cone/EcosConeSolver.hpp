5ef5c8bf7e554f2dbc380616725059a2c0243219
fabioftv <fabioftv@k-state.edu>
2016-06-24 16:06:47 -0500

    Updated EcosConeSolver

--- src/cpp/cvxcanon/solver/cone/EcosConeSolver.hpp
+++ src/cpp/cvxcanon/solver/cone/EcosConeSolver.hpp
@@ -1,6 +1,6 @@
-// Interface to the splitting conic solver (SCS)
+// Interface to the Embedded Conic Solver (ECOS)
 //
-// See: https://github.com/cvxgrp/scs
+// See: https://github.com/embotech/ecos
 //
 // TODO(mwytock): It is expected that this interface will be implemented
 // directly in the SCS code base at some point and called via a plugin
@@ -11,14 +11,57 @@
 
 #include <memory>
 #include <vector>
-
 #include "cvxcanon/solver/cone/ConeSolver.hpp"
 
-class EcosConeSolver : public ConeSolver {
- public:
-  ConeSolution solve(const ConeProblem& problem) override;
+// ECOS Environment 
+namespace ecos {
+	typedef double pfloat;
+#include <ecos/external/SuiteSparse_config/SuiteSparse_config.h>
+#include <ecos/include/ecos.h>
+}
 
- private:
+class EcosConeSolver : public ConeSolver {
+public:
+   EcosConeSolver();
+   ~EcosConeSolver();
+
+   ConeSolution solve(const ConeProblem& problem) override;
+private:
+   struct EcosData;
+
+   void build_ecos_problem(const ConeProblem& problem, ConeSolution* solution);
+
+   void build_ecos_constraint(
+		const Eigen::SparseMatrix<double, Eigen::RowMajor>& A,
+		const Eigen::SparseMatrix<double, Eigen::RowMajor>& G,
+		const DenseVector& b,
+		const DenseVector& h,
+		const std::vector<ConeConstraint>& constraints,
+		int* total_size,
+		int* sizes);
+
+   SolverStatus get_ecos_status();
+
+   // ECOS Data Structures
+   std::unique_ptr<EcosData> ecos_data_;
+
+   // ECOS Supporting Data Structures
+   // ecos::spmat A_matrix_;
+   // ecos::spmat G_matrix_;
+
+   // Constraints Ordered
+   SparseMatrix A_;
+   SparseMatrix G_;
+   DenseVector b_;
+   DenseVector h_;
+
+   // Extra Attributes for Constraints
+   int num_constrs_;
+   std::vector<Triplet> A_coeffs_;
+   std::vector<Triplet> G_coeffs_;
+
+   // Cone
+   // ecos::cone cone_;
 };
 
 #endif  // CVXCANON_SOLVER_CONE_ECOS_CONE_SOLVER_H
