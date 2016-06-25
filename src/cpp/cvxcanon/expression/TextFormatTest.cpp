4fd77eb7361ce1cdf60f20d61116bf36c4ae138a
fabioftv <fabioftv@k-state.edu>
2016-06-25 13:05:01 -0500

    Tests

--- src/cpp/cvxcanon/expression/TextFormatTest.cpp
+++ src/cpp/cvxcanon/expression/TextFormatTest.cpp
@@ -20,6 +20,6 @@ TEST(TextFormatTest, Basic) {
 
 TEST(TextFormatTest2, Second) {
   Expression x = constant(10.0);
-  EXPECT_EQ("const", format_expression(x));
+  EXPECT_EQ("constggg", format_expression(x));
 }
 
