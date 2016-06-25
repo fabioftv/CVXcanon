3b9e58eea3b810c095763a5665d8d31039225dc2
fabioftv <fabioftv@k-state.edu>
2016-06-25 14:10:27 -0500

    Tests

--- src/cpp/cvxcanon/expression/TextFormatTest.cpp
+++ src/cpp/cvxcanon/expression/TextFormatTest.cpp
@@ -14,9 +14,9 @@ TEST(TextFormatTest, Names) {
 }
 
 TEST(TextFormatTest, Basic) {
-//  Expression x = var(10, 5, 0);
-//  Expression y = var(20, 10, 0);
-  Expression add = (Expression x = var(10, 5, 0), Expression y = var(20, 10, 0));
+  Expression x = var(10, 5, 0);
+  Expression y = var(20, 10, 0);
+  Expression z = add(x, y);
 //  EXPECT_EQ("var", format_expression(x));
   EXPECT_EQ("add", format_expression(add));
 }
