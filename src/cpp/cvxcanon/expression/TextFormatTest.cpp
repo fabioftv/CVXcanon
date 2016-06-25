b6df6a6c01c1d3b7476117d49d1d671267643597
fabioftv <fabioftv@k-state.edu>
2016-06-25 15:03:53 -0500

    Tests

--- src/cpp/cvxcanon/expression/TextFormatTest.cpp
+++ src/cpp/cvxcanon/expression/TextFormatTest.cpp
@@ -16,6 +16,7 @@ TEST(TextFormatTest, Names) {
 TEST(TextFormatTest, FormatExpression) {
   Expression x = var(10, 5, 0);
   Expression y = var(20, 10, 0);
+  double d = 10.0;
 
   Expression add_vars = add(x, y);
   EXPECT_EQ("add(var, var)", format_expression(add_vars));
@@ -50,6 +51,68 @@ TEST(TextFormatTest, FormatExpression) {
   Expression trace_var = trace(x);
   EXPECT_EQ("trace(var)", format_expression(trace_var));
 
+  Expression quad_vars = quad_over_lin(x, y);
+  EXPECT_EQ("quad_over_lin(var, var)", format_expression(quad_vars));
+
+  Expression pnorm_vars = p_norm(x, d);
+  EXPECT_EQ("p_norm(var)", format_expression(pnorm_vars));
+
+/*
+
+
+Expression hstack(std::vector<Expression> args) {
+  return {Expression::HSTACK, args};
+}
+
+Expression vstack(std::vector<Expression> args) {
+  return {Expression::VSTACK, args};
+}
+*/
+/*
+  // Linear functions
+  {Expression::HSTACK, "hstack"},
+  {Expression::INDEX, "index"},
+  {Expression::KRON, "kron"},
+  {Expression::RESHAPE, "reshape"},
+  {Expression::SUM_ENTRIES, "sum_entries"},
+  {Expression::VSTACK, "vstack"},
+
+  // Elementwise functions
+  {Expression::ENTR, "entr"},
+  {Expression::HUBER, "huber"},
+  {Expression::KL_DIV, "kl_div"},
+  {Expression::LOG1P, "log1p"},
+  {Expression::LOGISTIC, "logistic"},
+  {Expression::MAX_ELEMWISE, "max_elemwise"},
+  {Expression::POWER, "power"},
+
+  // General nonlinear functions
+  {Expression::GEO_MEAN, "geo_mean"},
+  {Expression::LAMBDA_MAX, "lambda_max"},
+  {Expression::LOG_DET, "log_det"},
+  {Expression::LOG_SUM_EXP, "log_sum_exp"},
+  {Expression::MATRIX_FRAC, "matrix_frac"},
+  {Expression::MAX_ENTRIES, "max_entries"},
+  {Expression::NORM_NUC, "norm_nuc"},
+  {Expression::P_NORM, "p_norm"},
+  {Expression::QUAD_OVER_LIN, "quad_over_lin"},
+  {Expression::SIGMA_MAX, "sigma_max"},
+  {Expression::SUM_LARGEST, "sum_largest"},
+
+  // Constraints
+  {Expression::EQ, "eq"},
+  {Expression::EXP_CONE, "exp_cone"},
+  {Expression::LEQ, "leq"},
+  {Expression::SDP, "sdp"},
+  {Expression::SDP_VEC, "sdp_vec"},
+  {Expression::SOC, "soc"},
+
+  // Leaf nodes
+  {Expression::CONST, "const"},
+  {Expression::PARAM, "param"},
+  {Expression::VAR, "var"},
+
+*/
 
 
 //  EXPECT_EQ("var", format_expression(x));
